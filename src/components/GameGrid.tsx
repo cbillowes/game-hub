9d5a54161ab38d6e55bae09f937a034d1504750a
Clarice Bouwer <cbillowes@gmail.com>
2024-04-21 15:42:53 +0400

    Refactor: create useGames hook

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,32 +1,13 @@
-import { useEffect, useState } from "react";
-import apiClient from "../services/api-client";
 import {
   Alert,
   AlertDescription,
   AlertIcon,
   AlertTitle,
 } from "@chakra-ui/react";
-
-interface Game {
-  id: number;
-  name: string;
-}
-
-interface FetchGamesResponse {
-  count: number;
-  results: Game[];
-}
+import useGames from "../hooks/useGames";
 
 const GameGrid = () => {
-  const [games, setGames] = useState<Game[]>([]);
-  const [error, setError] = useState("");
-
-  useEffect(() => {
-    apiClient
-      .get<FetchGamesResponse>("/games2")
-      .then((response) => setGames(response.data.results))
-      .catch((error) => setError(error.message));
-  });
+  const { games, error } = useGames()
 
   return (
     <>
