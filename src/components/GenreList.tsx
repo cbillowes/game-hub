ca1511c6de65f0cd539f8846348d802868d7bf5b
Clarice Bouwer <cbillowes@gmail.com>
2024-04-22 19:53:39 +0400

    Add Genre with image

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,13 +1,24 @@
+import { HStack, Image, List, ListItem, Text } from "@chakra-ui/react";
 import useGenres from "../hooks/useGenres";
 
 const GenreList = () => {
   const { genres } = useGenres();
   return (
-    <ul>
-      {genres.map(({ id, name }) => (
-        <li key={id}>{name}</li>
+    <List>
+      {genres.map(({ id, name, image_background }) => (
+        <ListItem key={id} paddingY={2}>
+          <HStack>
+            <Image
+              boxSize="32px"
+              borderRadius={8}
+              src={image_background}
+              alt={name}
+            />
+            <Text fontSize="lg">{name}</Text>
+          </HStack>
+        </ListItem>
       ))}
-    </ul>
+    </List>
   );
 };
 
