27b499aaac435cff23c28559380d637d24b4a74f
Clarice Bouwer <cbillowes@gmail.com>
2024-04-22 20:06:38 +0400

    Skeleton loading genre names

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,9 +1,18 @@
-import { HStack, Image, List, ListItem, Text } from "@chakra-ui/react";
+import {
+  HStack,
+  Image,
+  List,
+  ListItem,
+  SkeletonText,
+  Text,
+} from "@chakra-ui/react";
 import useGenres from "../hooks/useGenres";
 import getCroppedImageUrl from "../services/image-url";
 
 const GenreList = () => {
-  const { genres } = useGenres();
+  const { genres, isLoading } = useGenres();
+  if (isLoading) return <SkeletonText noOfLines={8} spacing={4} />;
+
   return (
     <List>
       {genres.map(({ id, name, image_background }) => (
