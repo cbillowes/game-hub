66ed7b92b202906de5c106a27edc274f746e6857
Clarice Bouwer <cbillowes@gmail.com>
2024-04-27 10:20:51 +0400

    Create search input icon

--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -1,12 +1,18 @@
-import { Input } from "@chakra-ui/react";
+import { Input, InputGroup, InputLeftElement } from "@chakra-ui/react";
+import { BsSearch } from "react-icons/bs";
 
 const SearchInput = () => {
   return (
-    <Input
-      borderRadius={10}
-      placeholder="Eg. Grand Theft Auto"
-      variant="filled"
-    />
+    <InputGroup>
+      <InputLeftElement>
+        <BsSearch />
+      </InputLeftElement>
+      <Input
+        borderRadius={10}
+        placeholder="Eg. Grand Theft Auto"
+        variant="filled"
+      />
+    </InputGroup>
   );
 };
 
