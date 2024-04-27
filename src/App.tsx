03d56a81d127d204394b37b65d678b4ce40e7889
Clarice Bouwer <cbillowes@gmail.com>
2024-04-27 10:03:08 +0400

    Add sort ordering

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Grid, GridItem, Show } from "@chakra-ui/react";
+import { Grid, GridItem, HStack, Show } from "@chakra-ui/react";
 import NavBar from "./components/NavBar";
 import GameGrid from "./components/GameGrid";
 import GenreList from "./components/GenreList";
@@ -6,10 +6,12 @@ import PlatformSelector from "./components/PlatformSelector";
 import { useState } from "react";
 import { Genre } from "./hooks/useGenres";
 import { Platform } from "./hooks/usePlatforms";
+import SortSelector from "./components/SortSelector";
 
 export interface GameQuery {
   genre: Genre | null;
   platform: Platform | null;
+  sortOrder: string | null;
 }
 
 function App() {
@@ -35,13 +37,21 @@ function App() {
         </GridItem>
       </Show>
       <GridItem area="main">
-        <PlatformSelector
-          selectedPlatform={gameQuery.platform}
-          onSelectedPlatform={(platform) =>
-            setGameQuery({ ...gameQuery, platform })
-          }
-        />
-        <GameGrid gameQuery={gameQuery}/>
+        <HStack spacing={2} paddingLeft={2} marginBottom={2}>
+          <PlatformSelector
+            selectedPlatform={gameQuery.platform}
+            onSelectedPlatform={(platform) =>
+              setGameQuery({ ...gameQuery, platform })
+            }
+          />
+          <SortSelector
+            selectedSortOrder={gameQuery.sortOrder}
+            onSelectedSortOrder={(sortOrder) =>
+              setGameQuery({ ...gameQuery, sortOrder })
+            }
+          />
+        </HStack>
+        <GameGrid gameQuery={gameQuery} />
       </GridItem>
     </Grid>
   );
---
+++ src/components/SortSelector.tsx
@@ -0,0 +1,47 @@
+import { Button, Menu, MenuButton, MenuItem, MenuList } from "@chakra-ui/react";
+import { BsChevronCompactDown } from "react-icons/bs";
+
+export interface SortOption {
+  value: string;
+  label: string;
+}
+
+interface Props {
+  selectedSortOrder: string | null;
+  onSelectedSortOrder: (option: string) => void;
+}
+
+const SortSelector = ({ selectedSortOrder, onSelectedSortOrder }: Props) => {
+  const sortOrders = [
+    { value: "", label: "Relevance" },
+    { value: "-added", label: "Date added" },
+    { value: "name", label: "Name" },
+    { value: "-released", label: "Release date" },
+    { value: "-metacritic", label: "Popularity" },
+    { value: "-rating", label: "Average rating" },
+  ];
+
+  return (
+    <Menu>
+      <MenuButton as={Button} rightIcon={<BsChevronCompactDown />}>
+        Order by:{" "}
+        {selectedSortOrder
+          ? sortOrders.find((o) => o.value === selectedSortOrder)?.label
+          : "Relevance"}
+      </MenuButton>
+      <MenuList>
+        {sortOrders.map((option) => (
+          <MenuItem
+            key={option.value}
+            onClick={() => onSelectedSortOrder(option.value)}
+            value={option.value}
+          >
+            {option.label}
+          </MenuItem>
+        ))}
+      </MenuList>
+    </Menu>
+  );
+};
+
+export default SortSelector;
