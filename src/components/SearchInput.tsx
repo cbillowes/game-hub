8af7f87a657bc22f2d8194a66891b559dcd045ab
Clarice Bouwer <cbillowes@gmail.com>
2024-04-27 10:15:28 +0400

    Add skeleton search component

---
+++ src/components/SearchInput.tsx
@@ -0,0 +1,13 @@
+import { Input } from "@chakra-ui/react";
+
+const SearchInput = () => {
+  return (
+    <Input
+      borderRadius={10}
+      placeholder="Eg. Grand Theft Auto"
+      variant="filled"
+    />
+  );
+};
+
+export default SearchInput;
