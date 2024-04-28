9cfe3c82f5fec4abd8530a2114a02475a5d7b2eb
Clarice Bouwer <cbillowes@gmail.com>
2024-04-28 19:55:35 +0400

    Rename API client
    Include missing useData file

--- src/hooks/useData.ts
+++ src/hooks/useData.ts
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { AxiosRequestConfig, CanceledError } from "axios";
-import apiClient from "../services/api-client";
+import apiClient from "../services/apiClient";
 
 interface FetchResponse<T> {
   count: number;
