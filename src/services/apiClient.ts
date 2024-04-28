f96634b23ca16fbe7c18df0fc07c4b805dd53f7e
Clarice Bouwer <cbillowes@gmail.com>
2024-04-28 20:01:28 +0400

    Refactor API client

--- src/services/apiClient.ts
+++ src/services/apiClient.ts
@@ -1,8 +1,23 @@
 import axios from "axios";
 
-export default axios.create({
+const apiClient = axios.create({
   baseURL: import.meta.env.VITE_RAWG_IO_API_ENDPOINT,
   params: {
     key: import.meta.env.VITE_RAWG_IO_API_KEY,
-  }
+  },
 });
+
+class ApiClient<T> {
+  endpoint: string;
+
+  constructor(endpoint: string) {
+    this.endpoint = endpoint;
+  }
+
+  getAll = async () => {
+    const response = await apiClient.get<T[]>(this.endpoint);
+    return response.data;
+  };
+}
+
+export default ApiClient;
