e39a2160a6a1096e616c6223ff610e903aca7ec8
Clarice Bouwer <cbillowes@gmail.com>
2024-04-28 21:05:08 +0400

    Add initial data to genres hook

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,5 +1,6 @@
 import { useQuery } from "@tanstack/react-query";
 import genreService, { Genre } from "../services/genreService";
+import initialData from '../data/genres.json';
 
 const useGenres = () => {
   const {
@@ -9,6 +10,8 @@ const useGenres = () => {
   } = useQuery({
     queryKey: ["genres"],
     queryFn: genreService.getAll,
+    initialData,
+    staleTime: 1000 * 60 * 60 * 24, // 24 hours
   });
   return { genres, isLoading, error };
 };
